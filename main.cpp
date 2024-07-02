5ed23ef11e9010e46461505433cc7cb703d38906
Maksim Rafalchuk <qlolwtf@gmail.com>
2024-07-02 20:50:41 +0300

    init commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+﻿// Chat.cpp: определяет точку входа для приложения.
+//
+#include "main.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	for (int i = 1; i < argc; i++)
+	{
+		if (argv[i] == "-c")
+		{
+			std::cout << "run client side";
+		}
+		else
+		{
+			std::cout << "run server side";
+		}
+	}
+
+	return 0;
+}
